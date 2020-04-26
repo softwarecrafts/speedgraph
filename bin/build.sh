930018f3471d58ff45787adbb7afd66f1597c312
Andrew Miller <info@akmiller.co.uk>
2020-04-26 20:51:43 +0100

    double quotes

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 yarn
 
-export PATH='${SRC_DIR}/src/scripts':'${SRC_DIR}/node_modules/.bin':$PATH
+export PATH="${SRC_DIR}/src/scripts":"${SRC_DIR}/node_modules/.bin":$PATH
+
+echo $PATH
 
 speed_test && gatsby build && cp -r ./public/* $DEPLOY_DIR
