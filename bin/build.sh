a908a5be4c161e3772b2f626f8b686ecfe32a7db
Andrew Miller <info@akmiller.co.uk>
2020-04-26 19:05:04 +0100

    fix syntax errror

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 yarn
 
-export PATH='${SRC_DIR}/src/scripts':'${SRC_DIR}/node_modules/.bin'):$PATH
+export PATH='${SRC_DIR}/src/scripts':'${SRC_DIR}/node_modules/.bin':$PATH
 
 speed_test && gatsby build && cp -r ./public/* $DEPLOY_DIR
