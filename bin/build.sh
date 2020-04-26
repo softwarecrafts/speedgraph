da0b53ef5dfa8b2821bf53fb304dd4f56f578012
Andrew Miller <info@akmiller.co.uk>
2020-04-26 13:30:34 +0100

    add yarn to build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+yarn
+export PATH=:./node_modules/.bin:$PATH
 
-export PATH=./src/scripts:./node_modules/.bin:$PATH
-
-node speed_test.js && gatsby build && cp -r build $DEPLOY_DIR
+node ./src/scripts/speed_test.js && gatsby build && cp -r build $DEPLOY_DIR
