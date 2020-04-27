ad18ea184a1397d4078054e697bbd61846d6db23
Andrew Miller <info@akmiller.co.uk>
2020-04-27 21:02:12 +0100

    add data cleanup script

---
+++ bin/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DAYS_TO_KEEP_DATA=14
+DATA_DIR="${SRC_DIR}/data"
+
+DIR_TO_REMOVE=date --date="${DAYS_TO_KEEP_DATA} days ago" +y-%Y/m-%m/d-%d
+
+echo "rm -rf ${DATA_DIR}/${DIR_TO_REMOVE}"
