9d8899d69faf3b39e6814c4a5c19afa9008146ef
Andrew Miller <info@akmiller.co.uk>
2020-04-27 21:03:36 +0100

    store directory correctly

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -3,6 +3,6 @@
 DAYS_TO_KEEP_DATA=14
 DATA_DIR="${SRC_DIR}/data"
 
-DIR_TO_REMOVE=date --date="${DAYS_TO_KEEP_DATA} days ago" +y-%Y/m-%m/d-%d
+DIR_TO_REMOVE=`date --date="${DAYS_TO_KEEP_DATA} days ago" +y-%Y/m-%m/d-%d`
 
 echo "rm -rf ${DATA_DIR}/${DIR_TO_REMOVE}"
