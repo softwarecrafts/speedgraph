283bdd3fcf35f3a3f47297e4d90a8bd9199d4d5b
Andrew Miller <info@akmiller.co.uk>
2020-04-28 09:32:57 +0100

    fix build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@ yarn
 
 export PATH="${SRC_DIR}/src/scripts":"${SRC_DIR}/node_modules/.bin":$PATH
 
-speed_test && gatsby build && cp -r ./public/* $DEPLOY_DIR
+speed_test && cd ${SRC_DIR} && gatsby build && cp -r ./public/* $DEPLOY_DIR
